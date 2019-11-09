0945e8c3cf2022426649a63c5705552c356d2694
stjhin <silvialytjhin@Silvialys-MacBook-Pro.local>
2019-11-09 15:54:30 -0500

    toggle buttons

--- index.js
+++ index.js
@@ -6,8 +6,19 @@ $('#addNewItem').click(function(){
 });
 
 // Button - Delete Item from List will show the Delete Icon for every submitted items on the List
+$('#showDeleteIcons').click(function(){
+    $('.deleteIcon').toggleClass('showIcon');
+})
 
 // Operator - Cancel will toggle the input forms
 $('#cancelItem').click(function(){
     $('main.newItem').toggleClass('showNewItem');
 });
+
+// When User click Save Item to List, the arrays of datas from the input form should show up on the Table
+$('[input="submit"').click(function(){
+    $('#itemName')
+    $('#itemAmount')
+    $('#itemPrice')
+    $('#itemNote')
+})
