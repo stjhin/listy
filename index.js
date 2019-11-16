1ec671731e539850c3586c2a7ca06d33e33bb616
stjhin <silvialytjhin@Silvialys-MacBook-Pro.local>
2019-11-16 12:29:04 -0500

    append inputs onto the table gui

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Button - Make A New List will reset the list
 
 // Button - Add New Item will toggle and show the input forms
-$('#addNewItem').click(function(){
+$('#addNewItem, #cancelItem').click(function(){
     $('main.newItem').toggleClass('showNewItem');
+    // Input data has to be reset after every toggle
+    $('[data-itemID], [data-itemName], [data-itemAmount], [data-itemPrice], [data-itemNote]').val('');
 });
 
 // Button - Delete Item from List will show the Delete Icon for every submitted items on the List
@@ -11,14 +13,41 @@ $('#showDeleteIcons').click(function(){
 })
 
 // Operator - Cancel will toggle the input forms
-$('#cancelItem').click(function(){
-    $('main.newItem').toggleClass('showNewItem');
-});
+//$('#cancelItem').click(function(){
+//    $('main.newItem').toggleClass('showNewItem');
+//});
+
+let itemID = 1;
 
 // When User click Save Item to List, the arrays of datas from the input form should show up on the Table
-$('[input="submit"').click(function(){
-    $('#itemName')
-    $('#itemAmount')
-    $('#itemPrice')
-    $('#itemNote')
+$('#submitItem').click(function(event){
+    event.preventDefault();
+
+    // Get Value from the Input
+    //const itemID = $('#itemID').val();
+    const itemName = $('[data-itemName]').val();
+    const itemAmount = $('[data-itemAmount]').val();
+    const itemPrice = $('[data-itemPrice]').val();
+    const itemNote = $('[data-itemNote]').val();
+    // Append value onto the Table
+    $('[data-wholeList]').append(`
+    <tr>
+        <th><a class="delete deleteIcon"></a></th>
+        <th>${itemID}</th>
+        <td>${itemName}</td>
+        <td>${itemAmount}</td>
+        <td>$ ${itemPrice}</td>
+        <td>${itemNote}</td>
+    </tr>
+    `);
+
+    // Item ID should increment for every item, starting from 1
+    itemID++;
+
+    //
+})
+
+// Clear the submitted Items on the Table List
+$('#resetList').click(function(){
+    $('[data-wholeList]').val('');
 })
